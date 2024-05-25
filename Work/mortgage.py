8f680b65ca6c2b150990e888b11c84183d542337
Roshan-Abraham-rep <abraham.roshan8@gmail.com>
2024-05-25 15:34:42 +0200

    Update mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,18 +6,21 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 num_months = 0
-extra_payment_start_month = 1
-extra_payment_end_month = 12
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 extra_payment = 1000
 
 while principal > 0:
     num_months = num_months + 1
     if num_months >= extra_payment_start_month and  num_months <= extra_payment_end_month:
-        principal = principal * (1+rate/12) - payment + extra_payment
+        principal = principal * (1+rate/12) - payment - extra_payment
+        total_paid = total_paid + payment + extra_payment
     else:
+        if principal <= payment: 
+            principal = payment
         principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-    
+        total_paid = total_paid + payment
+
 print('Total paid', total_paid)
 print('Num of months', num_months)
 
